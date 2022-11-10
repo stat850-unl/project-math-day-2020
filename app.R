d97d6cd8b8eba061d247e47d36fc565dd171eda2
Molly Creagar <mcreagar2@huskers.unl.edu>
2022-11-10 15:18:37 -0600

    First app.R test

---
+++ app.R
@@ -0,0 +1,57 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+
+
+cleaned <- readr::read_csv("~/Downloads/cleaned.csv")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Song Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("song_name", "Song Name", choices = sort(unique(cleaned$SongName)), selected = "Hey, Soul Sister")
+    ),
+    
+    mainPanel(
+      #plotOutput("dist")
+      tableOutput("dist")
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  cln_subset <- reactive({
+    cleaned %>%
+      filter(SongName == input$song_name)
+  })
+  
+ # output$dist <- renderPlot({
+ #   cln_subset() %>%
+  #    select(dates, TimeInHours, TimeInPlays) %>%
+  #    ggplot(aes(x = dates, y = TimeInHours)) +
+  #    geom_point()
+    
+ # })
+  output$dist <- renderTable({
+    cln_subset() %>%
+      select(dates, TimeInHours, TimeInPlays)
+    
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
