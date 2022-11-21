0a30ca9f8bc50873b680801e1524dbd9684f9b92
Molly Creagar <mcreagar2@huskers.unl.edu>
2022-11-20 21:13:13 -0600

    Fixing slider to be double-sided

--- app.R
+++ app.R
@@ -15,181 +15,179 @@ library(tidyverse)
 cleaned <- readr::read_csv("~/Downloads/cleaned.csv")
 
 # Define UI for application
-ui <- fluidPage(theme = shinytheme("flatly"),
+ui <- fluidPage(
+  theme = shinytheme("flatly"),
   
   navbarPage(
-  # Application title
+    # Application title
     title = "SAAM",
-  
-  # Sidebar tabs
-  tabPanel(
-    "Songs",
     
-    tabsetPanel(
-      
-      type = "tabs",
-      
-      
-      tabPanel(
-        
-        "Overall Song Data",
-        
-        sidebarPanel(
-          sliderInput("topPlayed", "Hours", min = 0, max = 200, value = 100)
-        ),
-        
-        mainPanel(
-          tableOutput("play_info")
-        )
-      ),
-      
-      tabPanel(
-        
-        "Individual Song Data",
-        
-    sidebarPanel(
-      selectizeInput("song_name", "Song Name", choices = sort(unique(cleaned$SongName)), selected = "Instant Crush (feat. Julian Casablancas)", options = list(maxOptions = 10))
-    ),
+    # Sidebar tabs
+    tabPanel("Songs",
+             
+             tabsetPanel(
+               type = "tabs",
+               
+               
+               tabPanel(
+                 "Overall Song Data",
+                 
+                 sidebarPanel(sliderInput(
+                   "topPlayed",
+                   "Hours",
+                   min = 0,
+                   max = 200,
+                   value = c(0,70)
+                 )),
+                 
+                 mainPanel(tableOutput("play_info"))
+               ),
+               
+               tabPanel(
+                 "Individual Song Data",
+                 
+                 sidebarPanel(
+                   selectizeInput(
+                     "song_name",
+                     "Song Name",
+                     choices = sort(unique(cleaned$SongName)),
+                     selected = "Instant Crush (feat. Julian Casablancas)",
+                     options = list(maxOptions = 10)
+                   )
+                 ),
+                 
+                 mainPanel(
+                   tableOutput("song_info"),
+                   tableOutput("song_big4"),
+                   plotOutput("dateTimeListen"),
+                   plotOutput("dateTimeListenNC")
+                 )
+               )
+             )),
     
-    mainPanel(
-      tableOutput("song_info"),
-      tableOutput("song_big4"),
-      plotOutput("dateTimeListen"),
-      plotOutput("dateTimeListenNC")
-    )
+    
+    # Second big tab
+    tabPanel("Artists",
+             
+             tabsetPanel(
+               type = "tabs",
+               
+               
+               tabPanel(
+                 "Individual Artist Data",
+                 
+                 sidebarPanel(
+                   selectizeInput(
+                     "artist_name",
+                     "Artist",
+                     choices = sort(unique(cleaned$Artist)),
+                     selected = "Daft Punk",
+                     options = list(maxOptions = 10)
+                   )
+                 ),
+                 
+                 mainPanel(tableOutput("artist_info"))
+               )
+             )),
+    
+    
+    tabPanel("Albums",
+             
+             tabsetPanel(type = "tabs",
+                         tabPanel("Album Info"))),
+    
+    
+    tabPanel("Miscellaneous",
+             
+             tabsetPanel(type = "tabs",
+                         
+                         tabPanel(
+                           "Bourgeoisie by Month"
+                           
+                         )))
   )
-      )
-    ),
-  
-  
-  # Second big tab
-  tabPanel(
-    "Artists",
-    
-    tabsetPanel(
-      
-      type = "tabs",
-      
-      
-      tabPanel(
-        
-        "Individual Artist Data",
-        
-        sidebarPanel(
-          selectizeInput("artist_name", "Artist", choices = sort(unique(cleaned$Artist)), selected = "Daft Punk", options = list(maxOptions = 10))
-        ),
-        
-        mainPanel(
-          tableOutput("artist_info")
-        )
-      )
-    )
-  ),
-
-
-tabPanel(
-  "Albums",
-  
-  tabsetPanel(
-    type = "tabs",
-    tabPanel(
-      "Album Info"
-    )
-  )
-),
-
-
-tabPanel(
-  "Miscellaneous",
-  
-  tabsetPanel(
-    type = "tabs",
-    
-    tabPanel(
-      "Bourgeoisie by Month"
-      
-    )
-  )
-)
-)
 )
 
 server <- function(input, output, session) {
   
+  ###### SONGS ###########
   
-  ## SONGS
   subset_plays <- reactive({
     cleaned %>%
       filter(!is.na(TimeInHours),
-             TimeInHours >= input$topPlayed)
+             TimeInHours >= input$topPlayed[1],
+             TimeInHours <= input$topPlayed[2])
   })
   
   cln_subset <- reactive({
     cleaned %>%
       filter(SongName == input$song_name) %>%
-      mutate(datenum=as.Date(dates, format = "%B %d %Y"))
+      mutate(datenum = as.Date(dates, format = "%B %d %Y"))
   })
   
   cln_subset_overall_info <- reactive({
     cleaned %>%
       filter(SongName == input$song_name) %>%
       filter(!is.na(TimeInHours)) %>%
-      summarize(MaximumRank = max(MaxRank), TotalHoursListened = max(TimeInHours), TotalPlays = max(TimeInPlays), ArtistName = unique(Artist), AlbumName = unique(Album))
- 
+      summarize(
+        MaximumRank = as.integer(max(MaxRank)),
+        ArtistName = unique(Artist),
+        AlbumName = unique(Album)
+      )
+    
   })
   
-  cln_big_4_song <- reactive({
-    cleaned %>%
-      filter(SongName == input$song_name) %>%
-      filter(!is.na(TimeInHours)) %>%
-      slice_tail(n=1)
+  # overall
+  output$play_info <- renderTable({
+    subset_plays() %>%
+      select(SongName, Artist, TimeInHours) %>%
+      group_by(SongName) %>%
+      summarise(TotalTime = max(TimeInHours)) %>%
+      arrange(desc(TotalTime))
   })
   
-  
-
+  # individual
   output$song_info <- renderTable({
     cln_subset_overall_info() %>%
-     select(MaximumRank, TotalHoursListened, TotalPlays, ArtistName, AlbumName)
+      select(MaximumRank, ArtistName, AlbumName)
   })
   
   output$song_big4 <- renderTable({
-    cln_big_4_song() %>%
-      select(RankOutOfN, ValueOutOf1, TimeInHours,TimeInPlays)
+    cln_subset() %>%
+      filter(!is.na(TimeInHours)) %>%
+      slice_tail(n = 1) %>%
+      select(RankOutOfN, ValueOutOf1, TimeInHours, TimeInPlays)
   })
   
   output$dateTimeListen <- renderPlot({
-     cln_subset() %>%
-     select(datenum, TimeInHours) %>%
+    cln_subset() %>%
+      select(datenum, TimeInHours) %>%
       filter(!is.na(TimeInHours)) %>%
       ggplot(aes(x = datenum, y = TimeInHours)) +
       geom_point() +
-      labs(x = "Date", y = "Cumulative Hours Listened", title="Cumulative Hours Listened")
+      labs(x = "Date", y = "Cumulative Hours Listened", title = "Cumulative Hours Listened")
+  })
+  
+  output$dateTimeListenNC <-
+    renderPlot({
+      #non-cumulative listening hours
+      cln_subset() %>%
+        select(datenum, dHours) %>%
+        filter(!is.na(dHours)) %>%
+        ggplot(aes(x = datenum, y = dHours)) +
+        geom_line() +
+        labs(x = "Date", y = "Hours Listened By Date", title = "Hours Listened by Date Added")
     })
   
-  output$dateTimeListenNC <- renderPlot({ #non-cumulative listening hours
-    cln_subset() %>%
-      select(datenum, dHours) %>%
-      filter(!is.na(dHours)) %>%
-      ggplot(aes(x = datenum, y = dHours)) +
-      geom_line() +
-      labs(x = "Date", y = "Hours Listened By Date", title = "Hours Listened by Date Added")
-  })
-  
-  output$play_info <- renderTable({
-   subset_plays() %>%
-      select(SongName, Artist, TimeInHours) %>%
-      group_by(SongName) %>%
-      summarise(TotalTime = max(TimeInHours))
-  })
   
   
   
-  ### ARTISTS
+  ########## ARTISTS #############
   
   cln_artist_subset <- reactive({
     cleaned %>%
       filter(Artist == input$artist_name) %>%
-      mutate(datenum=as.Date(dates, format = "%B %d %Y"),
+      mutate(datenum = as.Date(dates, format = "%B %d %Y"),
              MaxRank = as.integer(MaxRank))
   })
   
@@ -198,24 +196,31 @@ server <- function(input, output, session) {
       filter(Artist == input$artist_name) %>%
       filter(!is.na(TimeInHours)) %>%
       mutate(MaxRank = as.integer(MaxRank))
-  
+    
     
   })
   
-
+  
   
   output$artist_info <- renderTable({
-      cln_artist_overall_info() %>%
+    cln_artist_overall_info() %>%
       select(SongName, Album, MaxRank) %>%
       group_by(SongName) %>%
       summarise()
-
+    
   })
   
-
   
+  ########## ALBUMS #############
+  
+  
+  
+  
+  
+  
+  ########## MISCELLANEOUS #############
   
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
